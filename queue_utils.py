f79f647b925bc2c71c40430d1e7fd78ea78bbd16
NoahWhite1115 <nawhite1115@gmail.com>
2021-07-11 23:16:16 -0700

    Added queuing

---
+++ queue_utils.py
@@ -0,0 +1,37 @@
+import boto3
+from botocore.exceptions import ClientError
+import logging
+
+sqs = boto3.resource('sqs')
+logger = logging.getLogger(__name__)
+
+def get_queue(name):
+    try:
+        queue = sqs.get_queue_by_name(QueueName=name)
+        logger.info("Got queue '%s' with URL=%s", name, queue.url)
+    except ClientError as error:
+        logger.exception("Couldn't get queue named %s.", name)
+        raise error
+    else:
+        return queue
+
+def send_to_queue(queue, messageBody, messageKeys = None):
+    if not messageKeys:
+        messageKeys = {}
+
+    try:
+        response = queue.send_message(
+            MessageBody=messageBody,
+            MessageAttributes=messageKeys
+        )
+    except ClientError as error:
+        logger.exception("Send message failed: %s", messageBody)
+        raise error
+    else:
+        return response
+
+def test():
+    queue = get_queue('test_queue')
+    send_to_queue(queue, "Hello")
+
+test()
--- testapp.py
+++ testapp.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
+from queue_utils import get_queue, send_to_queue
 
 app = Flask(__name__)
+queue = get_queue('test_queue')
 
 @app.route('/payload', methods = ['POST', 'GET'])
 def handleWebhook():
@@ -8,6 +10,8 @@ def handleWebhook():
         result = request.get_json()
         print("Got a post request")
         print(result)
+        send_to_queue(queue, result)
+
         return "success"
 
 if __name__ == '__main__':
